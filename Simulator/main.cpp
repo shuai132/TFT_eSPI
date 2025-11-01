3cd095d173a10e80e6a0dab7c84b232597a46780
liushuai <770722922@qq.com>
2025-11-01 21:00:01 +0800

    feat: impl loop

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -1,4 +1,5 @@
 #include "TFT_eSPI.h"
+#include "fake/INA226.hpp"
 #include "render_sdl.h"
 
 #include "RingBuffer.hpp"
@@ -22,25 +23,26 @@ TFT_eSprite g_spr(&g_tft);
 ScreenConfig g_screen_config;
 RingBuffer<InaMsg, UI_CHART_WIDTH> g_sensor_buffer;
 
-static void drawTest() {
-  g_spr.createSprite(SCREEN_WIDTH, SCREEN_HEIGHT);
-  ui_value_update();
-  ui_chart_update();
-}
+static fake::INA226 fake_ina;
 
 int main() {
   SDLRenderer renderer(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_SCALE);
   if (!renderer.init()) {
     return 1;
   }
-
-  // test
-  drawTest();
-
-  // update display
-  renderer.updateDisplay(&g_spr, 0, 0);
+  g_spr.createSprite(SCREEN_WIDTH, SCREEN_HEIGHT);
 
   // run event loop
-  SDLRenderer::runEventLoop();
+  SDLRenderer::runEventLoop([&renderer] {
+    g_ina_current.v = fake_ina.getBusVoltage();
+    g_ina_current.c = fake_ina.getCurrent();
+    g_ina_current.p = fake_ina.getPower();
+    g_sensor_buffer.push_back(g_ina_current);
+
+    g_spr.fillSprite(SCREEN_BG);
+    ui_value_update();
+    ui_chart_update();
+    renderer.updateDisplay(&g_spr, 0, 0);
+  });
   return 0;
 }
